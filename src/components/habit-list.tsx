ac2e37c164a2f106667434aa629582aff6fd0ea9
lakshay17 <lakshayjain01lj@gmail.com>
2025-04-09 12:52:39 +0530

    feat: enhance HabitList with completion and removal functionality

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,27 +1,48 @@
 import { Box, Button, Grid, Paper, Typography } from "@mui/material"
 import useHabitStore from "../store/store"
+import { CheckCircle, Delete } from "@mui/icons-material"
 
 export const HabitList = () => {
 
-    const { habits } = useHabitStore()
+    const { habits, removeHabit } = useHabitStore()
 
-    return <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
-        {habits.map((habit) => (
-            <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
-                <Grid container alignContent="center">
-                    <Grid item={true} xs={12} sm={6}>
-                        <Typography variant="h6">{habit.name}</Typography>
-                        <Typography variant="body2" color="text.secondary">{habit.frequency}</Typography>
-                    </Grid>
+    const today = new Date().toISOString().split("T")[0]
 
-                    <Grid item={true} xs={12} sm={6}>
-                        <Box sx={{display:"flex", justifyContent: "flex-end", gap:1}}>
-                            <Button variant="outlined">Mark as complete</Button>
-                            <Button variant="outlined" color="error">Remove</Button>
-                        </Box>
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
+            {habits.map((habit) => (
+                <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid size={{ xs: 12, sm: 6 }}>
+                            <Typography variant="h6">{habit.name}</Typography>
+                            <Typography variant="body2" color="text.secondary">{habit.frequency}</Typography>
+                        </Grid>
+
+                        <Grid size={{ xs: 12, sm: 6 }}>
+                            <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+                                <Button
+                                    variant="outlined"
+                                    color={habit.completedDates.includes(today)
+                                        ? "success"
+                                        : "primary"
+                                    }
+                                    startIcon={<CheckCircle />}
+                                >
+                                    {habit.completedDates.includes(today)
+                                        ? "Completed"
+                                        : "Mark as Completed"}
+                                </Button>
+
+                                <Button variant="outlined"
+                                    color="error"
+                                    startIcon={<Delete />}
+                                    onClick={removeHabit(habit.id)}
+                                >Remove</Button>
+                            </Box>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Paper>
-        ))}
-    </Box>
+                </Paper>
+            ))}
+        </Box>
+    )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -10,10 +10,12 @@ export interface Habit {
 
 interface HabitState {
     habits: Habit[];
-    addHabit: (name: string, frequency: "daily" | "weekly") => void
+    addHabit: (name: string, frequency: "daily" | "weekly") => void;
+    removeHabit: (id: string) => void;
+    toggleHabit: (id: string, date: string) => void;
 }
 
-const useHabitStore = create<HabitState>((set, get) => {
+const useHabitStore = create<HabitState>((set) => {
     return {
         habits: [],
         addHabit: (name, frequency) => set((state) => {
@@ -26,6 +28,26 @@ const useHabitStore = create<HabitState>((set, get) => {
                     createdAt: new Date().toISOString()
                 }],
             }
+        }),
+
+        removeHabit: (id) => set((state)=>({
+            habits: state.habits.filter(habit => habit.id !== id),
+        })),
+
+        toggleHabit: (id, date) => set((state) => {
+            const habitIndex = state.habits.findIndex(habit => habit.id === id);
+            if (habitIndex !== -1) {
+                const updatedHabit = { ...state.habits[habitIndex] };
+                if (updatedHabit.completedDates.includes(date)) {
+                    updatedHabit.completedDates = updatedHabit.completedDates.filter(d => d !== date);
+                } else {
+                    updatedHabit.completedDates.push(date);
+                }
+                const updatedHabits = [...state.habits];
+                updatedHabits[habitIndex] = updatedHabit;
+                return { habits: updatedHabits };
+            }
+            return state;
         })
     }
 })
